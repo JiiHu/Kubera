c347d57c2b3f6d7c146801450dab4236c064689e
Jonne <jonne.huotari@cs.helsinki.fi>
2015-04-22 16:50:00 +0300

    bugs fixd

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,6 +5,10 @@ class Entry < ActiveRecord::Base
 	validates_presence_of :category_id
 	validates_presence_of :user_id
 
+	validates :amount, :numericality => true, :allow_nil => true
+	validates :category_id, :numericality => true, :allow_nil => true
+	validates :user_id, :numericality => true, :allow_nil => true
+
 	belongs_to :user
 	belongs_to :category
 
